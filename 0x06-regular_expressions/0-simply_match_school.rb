010aca504815a72b506eb9e220c2e0918999bf60
abdu mk <mk22malika@gmail.com>
2023-05-05 21:49:36 -0700

    task 0 udated

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,2 +1,16 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/School/).join
+
+regex = /School/
+
+if ARGV.length != 1
+  puts "Usage: #{$PROGRAM_NAME} <string>"
+  exit 1
+end
+
+string = ARGV[0]
+
+if regex.match?(string)
+  puts "School"
+else
+  puts string
+end
